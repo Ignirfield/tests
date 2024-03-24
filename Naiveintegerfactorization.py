23a28a45a3633bf44316f1a5f49f24cafbd1ead5
Ignirfield <163201587+Ignirfield@users.noreply.github.com>
2024-03-24 15:22:28 +0100

    Add files via upload

---
+++ Naiveintegerfactorization.py
@@ -0,0 +1,32 @@
+def isprime(b):
+    o=0
+    for i in range(1,(b//2)+1):
+        if b%i==0:
+            o+=1
+    if o==1:  
+        return True
+    else:
+        return False
+    
+    
+
+def factorization(A):
+    L=[]
+    G=[]
+    if isprime(A)==True:
+        return 'A is already a prime number'
+    else:
+        
+        for i in range(2,(A//2)+1):
+            for k in range(1,10):
+            
+                if A%(i**k)==0 and isprime(i)==True:
+                    L.append((i,k)) 
+    
+        for nombre in range(len(L)-1):
+                if L[nombre][0]!=L[nombre+1][0]:
+                    G.append(L[nombre])
+        G.append(L[-1])
+    return G
+        
+print(factorization(1277))
